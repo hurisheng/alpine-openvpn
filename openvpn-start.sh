10d444bc89ce5079eedd40aa606cd35dc8a982af
hurisheng <hurisheng@gmail.com>
2017-10-28 01:09:21 +0800

    update execute script

--- openvpn-start.sh
+++ openvpn-start.sh
@@ -1,6 +1,11 @@
 #!/bin/sh
 
-mkdir -p /dev/net
-mknod /dev/net/tun c 10 200
+if [ ! -d /dev/net ]; then
+    mkdir -p /dev/net
+fi
+if [ ! -c /dev/net/tun ]; then
+    mknod /dev/net/tun c 10 200
+    chmod 0666 /dev/net/tun
+fi
 
-openvpn --cd /etc/openvpn --config /etc/openvpn/server.conf
+openvpn --cd /etc/openvpn --config server.conf
